6f72b726c3dfca1c1c81853fd01847106a3b7890
Eric Militello <ericmilitellocoding@gmail.com>
2023-04-06 14:46:27 -0400

    added size feilds for paitning db model

--- jimTheArtist/models.py
+++ jimTheArtist/models.py
@@ -5,6 +5,8 @@ from django.db import models
 class Painting(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
+    length = models.IntegerField()
+    height = models.IntegerField()
     price = models.IntegerField()
     image = models.ImageField(upload_to='paintings/')
 
