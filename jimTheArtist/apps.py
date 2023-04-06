7f6a5fcb39c80f89f2848168590d49d4ce8e5c3f
Eric Militello <ericmilitellocoding@gmail.com>
2023-04-06 13:42:27 -0400

    added form control so none of the form overhangs

--- jimTheArtist/apps.py
+++ jimTheArtist/apps.py
@@ -7,6 +7,6 @@ class JimtheartistConfig(AppConfig):
     name = 'jimTheArtist'
 
 class ContactForm(forms.Form):
-    name = forms.CharField(label='Your Name', max_length=100)
-    email = forms.EmailField(label='Your Email')
-    message = forms.CharField(label='Your Message', widget=forms.Textarea)
+    name = forms.CharField(label='Your Name', max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    email = forms.EmailField(label='Your Email', widget=forms.EmailInput(attrs={'class': 'form-control'}))
+    message = forms.CharField(label='Your Message', widget=forms.Textarea(attrs={'class': 'form-control'}))
