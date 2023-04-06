f7134223c4a06e48435a3e5fe0cdace8f80e911f
Eric Militello <ericmilitellocoding@gmail.com>
2023-04-06 14:51:39 -0400

    contact form styling

--- jimTheArtist/apps.py
+++ jimTheArtist/apps.py
@@ -7,6 +7,6 @@ class JimtheartistConfig(AppConfig):
     name = 'jimTheArtist'
 
 class ContactForm(forms.Form):
-    name = forms.CharField(label='Your Name', max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
-    email = forms.EmailField(label='Your Email', widget=forms.EmailInput(attrs={'class': 'form-control'}))
-    message = forms.CharField(label='Your Message', widget=forms.Textarea(attrs={'class': 'form-control'}))
+    name = forms.CharField(label='Name:', max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    email = forms.EmailField(label='Email:', widget=forms.EmailInput(attrs={'class': 'form-control'}))
+    message = forms.CharField(label='Message:', widget=forms.Textarea(attrs={'class': 'form-control'}))
